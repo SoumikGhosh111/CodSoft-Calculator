d532af39cb96467635d0b0956800dd3b7ec806ab
SoumikGhosh111 <132730107+SoumikGhosh111@users.noreply.github.com>
2023-08-09 14:00:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let output = ""; 
+let buttons = document.querySelectorAll('.button'); 
+Array.from(buttons).forEach(button => {
+    button.addEventListener('click', (e)=>{
+        if( e.target.innerHTML == '='){ 
+            output = eval(output);
+            document.querySelector('.output').innerHTML = output;  
+        }
+        else if( e.target.innerHTML == "AC"){ 
+            output = ""
+            document.querySelector('.output').innerHTML = output;  
+        }
+        else if( e.target.innerHTML == '×'){ 
+            output = output + "*"; 
+            document.querySelector('.output').innerHTML = output;  
+        }
+        else if( e.target.innerHTML == '−'){ 
+            output = output + "-"; 
+            document.querySelector('.output').innerHTML = output;  
+        }
+        else if( e.target.innerHTML == '÷'){ 
+            output = output + "/"; 
+            document.querySelector('.output').innerHTML = output;  
+        }
+        else if( e.target.innerHTML == 'C'){ 
+            output = output.substring(0, output.length-1);  
+            document.querySelector('.output').innerHTML = output;  
+        }
+       else{ 
+        output = output + e.target.innerHTML; 
+        document.querySelector('.output').innerHTML = output; 
+       }
+    }); 
+});
